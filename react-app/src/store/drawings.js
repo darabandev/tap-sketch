19948b758e4e37dc84c73150e32ebe219d5c2066
rob <darabandev@gmail.com>
2021-03-04 12:33:20 -0500

    Created thunk to post new drawing

---
+++ react-app/src/store/drawings.js
@@ -0,0 +1,41 @@
+const NEW_DRAWING = "drawings/NEW_DRAWING";
+
+export const newDrawing = drawing => {
+  return {
+    type: NEW_DRAWING,
+    payload: drawing,
+  };
+};
+
+export const createNewDrawing = drawing => async dispatch => {
+  const { userId, caption, dataUri } = drawing;
+
+  const res = await fetch(`/api/drawings/create`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      user_id: userId,
+      caption,
+      data_uri: dataUri,
+    }),
+  });
+
+  const data = await res.json();
+
+  dispatch(newDrawing(data));
+};
+
+const initialState = { currentDrawing: null, allDrawings: null };
+
+export default function sessionReducer(state = initialState, action) {
+  const updateState = { ...state };
+  switch (action.type) {
+    case NEW_DRAWING:
+      updateState.currentDrawing = action.payload;
+      return updateState;
+    default:
+      return state;
+  }
+}
