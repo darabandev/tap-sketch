648a1ff1e2c1dab6ae2cdf450dc506d7dbaa4f43
rob <darabandev@gmail.com>
2021-03-06 12:01:51 -0500

    Improved drawing view layout

--- react-app/src/components/DrawingView/index.js
+++ react-app/src/components/DrawingView/index.js
@@ -1,15 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import dateConverter from "../../services/dateConverter";
 import "./DrawingView.css";
 
 const DrawingView = ({ drawing }) => {
   return (
     <div>
       <img className="drawing-img" src={drawing.data_url} alt="drawing" />
+      <div className="drawing-items">
+        <div>
+          <button className="drawing-btn">
+            <i class="far fa-heart"></i>
+          </button>
+          <button className="drawing-btn">
+            <i class="far fa-comment"></i>
+          </button>
+        </div>
+        <p className="drawing-date">{dateConverter(drawing.created_at)}</p>
+      </div>
       <p>
-        <span className="drawing-username">{drawing.username}</span>
+        <Link style={{ textDecoration: "none" }} to={`/profile/${drawing.username}`}>
+          <span className="drawing-username">{drawing.username}</span>
+        </Link>
         <span className="drawing-caption"> {drawing.caption}</span>
       </p>
-      <p>{drawing.created_at}</p>
     </div>
   );
 };
