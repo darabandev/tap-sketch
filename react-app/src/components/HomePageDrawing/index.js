32a53f278ba85f4170a13e2c04ab04b166945aa1
rob <darabandev@gmail.com>
2021-03-09 18:18:27 -0500

    Home page card looking good

--- react-app/src/components/HomePageDrawing/index.js
+++ react-app/src/components/HomePageDrawing/index.js
@@ -6,19 +6,29 @@ const HomePageDrawing = ({ drawing }) => {
   return (
     <div className="drawing-card">
       <div className="card-above-img-container">
-        <img className="card-profile-img" src="https://i.imgur.com/xAcplDO.jpg" />
-        <p className="card-profile-username">{drawing.username}</p>
+        <Link to={`/profile/${drawing.username}`}>
+          <img className="card-profile-img" src="https://i.imgur.com/xAcplDO.jpg" alt="profile" />
+        </Link>
+        <Link to={`/profile/${drawing.username}`}>
+          <p className="card-profile-username">{drawing.username}</p>
+        </Link>
       </div>
       <Link to={`/drawings/${drawing.id}`}>
         <img className="card-img" src={drawing.data_url} alt="drawing" />
       </Link>
       <div className="card-below-img-container">
-        <span className="card-profile-username">{drawing.username}</span>
-        <span>{drawing.caption}</span>
+        <Link to={`/profile/${drawing.username}`}>
+          <span className="card-profile-username">{drawing.username}</span>
+        </Link>
+        <span className="card-caption">{drawing.caption}</span>
       </div>
       <div className="card-likes-comments">
-        <span>{drawing.likes} Likes</span>
-        <span>{drawing.comments} Comments</span>
+        <span className="card-likes">
+          {drawing.likes} {drawing.likes === 1 ? "Like" : "Likes"}
+        </span>
+        <span>
+          {drawing.comments} {drawing.comments === 1 ? "Comment" : "Comments"}
+        </span>
       </div>
     </div>
   );
