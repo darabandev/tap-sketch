6be31e0eac0ec65aba9e76bf3125f3f07a139510
rob <darabandev@gmail.com>
2021-03-09 18:06:12 -0500

    General layout of drawing card coming together

--- react-app/src/components/HomePageDrawing/index.js
+++ react-app/src/components/HomePageDrawing/index.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./HomePageDrawing.css";
 
 const HomePageDrawing = ({ drawing }) => {
   return (
-    <div>
-      <h1>{drawing.caption}</h1>
-      <h1>{drawing.username}</h1>
+    <div className="drawing-card">
+      <div className="card-above-img-container">
+        <img className="card-profile-img" src="https://i.imgur.com/xAcplDO.jpg" />
+        <p className="card-profile-username">{drawing.username}</p>
+      </div>
+      <Link to={`/drawings/${drawing.id}`}>
+        <img className="card-img" src={drawing.data_url} alt="drawing" />
+      </Link>
+      <div className="card-below-img-container">
+        <span className="card-profile-username">{drawing.username}</span>
+        <span>{drawing.caption}</span>
+      </div>
+      <div className="card-likes-comments">
+        <span>{drawing.likes} Likes</span>
+        <span>{drawing.comments} Comments</span>
+      </div>
     </div>
   );
 };
