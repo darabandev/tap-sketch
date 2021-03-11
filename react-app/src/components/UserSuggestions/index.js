a482427a5b2804fabfb5a6bf46ed7487a3d3c361
rob <darabandev@gmail.com>
2021-03-11 13:32:24 -0500

    User suggestions appearing

--- react-app/src/components/UserSuggestions/index.js
+++ react-app/src/components/UserSuggestions/index.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./UserSuggestions.css";
 
 const UserSuggestions = () => {
   return (
-    <div>
+    <div className="user-suggestions">
       <h1>Looks like you aren't following anyone...</h1>
+      <h2>Get started with some of these users!</h2>
+      <p>
+        <Link to="/profile/robd">robd</Link>
+      </p>
+      <p>
+        <Link to="/profile/blackandyellow">blackandyellow</Link>
+      </p>
+      <p>
+        <Link to="/profile/theartiste">theartiste</Link>
+      </p>
     </div>
   );
 };
