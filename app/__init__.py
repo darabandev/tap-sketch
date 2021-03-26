255978fbefe1a3737fcdc98e03b97e6f76c73ffd
rob <darabandev@gmail.com>
2021-03-26 18:34:29 -0400

    Added meta image tag

--- app/__init__.py
+++ app/__init__.py
@@ -73,6 +73,6 @@ def inject_csrf_token(response):
 @app.route('/<path:path>')
 def react_root(path):
     print("path", path)
-    if path == 'favicon.ico':
-        return app.send_static_file('favicon.ico')
+    if path in ('favicon.ico', 'manifest.json', 'screencap.png'):
+        return app.send_static_file(path)
     return app.send_static_file('index.html')
